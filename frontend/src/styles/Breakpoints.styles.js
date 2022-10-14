f470b65d6ae6c4010af8b70cc0bcf73b0ea65675
Tonmoy Roy <rxdevs100@gmail.com>
2022-10-14 22:04:32 +0600

    media queries added & banner layout updated

---
+++ frontend/src/styles/Breakpoints.styles.js
@@ -0,0 +1,34 @@
+// define breakpoints
+const sizes = {
+    sm: '600px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px'
+}
+
+// define devices
+export const devices = {
+    device_xs: `(max-width:${sizes.sm})`,
+    device_sm: `(min-width:${sizes.sm})`,
+    device_md: `(min-width:${sizes.md})`,
+    device_lg: `(min-width:${sizes.lg})`,
+    device_xl: `(min-width:${sizes.xl})`,
+}
+
+
+
+// /* Extra small devices (phones, 600px and down) */
+// @media only screen and (max-width: 600px) {...}
+
+// /* Small devices (portrait tablets and large phones, 600px and up) */
+// @media only screen and (min-width: 600px) {...}
+
+// /* Medium devices (landscape tablets, 768px and up) */
+// @media only screen and (min-width: 768px) {...}
+
+// /* Large devices (laptops/desktops, 992px and up) */
+// @media only screen and (min-width: 992px) {...}
+
+// /* Extra large devices (large laptops and desktops, 1200px and up) */
+// @media only screen and (min-width: 1200px) {...}
+
