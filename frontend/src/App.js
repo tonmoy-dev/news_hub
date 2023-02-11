4559403408c39bc7ac4e6324031a684bd62a2552
Tonmoy Roy <rxdevs100@gmail.com>
2023-02-11 20:55:12 +0600

    updated files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Explore from './components/Explore/Explore';
+import Footer from './components/Footer/Footer';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Explore/>
+      <Footer/>
+      {/* <Login/> */}
     </div>
   );
 }
